74470d92a2e877947d7e7c2094d185300243374d
Vipender <vipenderyadav1@gmail.com>
2025-08-30 04:12:10 +0000

    Auto-commit on Sat Aug 30 04:12:10 UTC 2025

--- auto_commit.sh
+++ auto_commit.sh
@@ -12,3 +12,10 @@ do
 
     sleep 300  # Wait 300 seconds (5 minutes)
 done
+
+"""
+# This script automatically commits and pushes changes to a Git repository every 5 minutes.
+# It first stages all changes, checks if there are any changes to commit, and if so, it commits them with a timestamped message and pushes to the remote repository.
+#
+# Usage:./auto_commit.sh
+"""
